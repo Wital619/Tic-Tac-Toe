8165784da9c93e9dd4a9c09b3bb6351559166cf3
Vitaliy Pogoretskyy <vitpogoretsky@gmail.com>
2018-11-05 16:45:45 +0200

    Add and configure all main files

---
+++ stylelint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard"
+  ],
+  plugins: [
+		"stylelint-scss"
+	],
+  rules: {
+		"at-rule-no-unknown": null,
+		"scss/at-rule-no-unknown": true,
+    indentation: 2,
+    "number-leading-zero": null,
+    "selector-pseudo-class-no-unknown": [true, {
+      ignorePseudoClasses: ["global"]
+    }],
+    "selector-max-id": 0,
+    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$"
+  }
+};
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/js/main.js'],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions'
+                  ]
+                }),
+              ]
+            },
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('public'),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./index.html')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: 'public',
+    overlay: true,
+    open: true,
+    openPage: 'index.html',
+  }
+};
