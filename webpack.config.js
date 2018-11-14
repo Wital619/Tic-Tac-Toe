8e5c66ecfb4068cc6fb62276c36944ede5e40c81
Vitaliy Pogoretskyy <vitpogoretsky@gmail.com>
2018-11-15 01:10:05 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,63 +5,69 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const path = require('path');
 
-module.exports = {
-  entry: ['./src/js/main.js'],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'cheap-module-eval-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(css|sass|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                autoprefixer({
-                  browsers: [
-                    '>1%',
-                    'last 4 versions'
-                  ]
-                }),
-              ]
-            },
-          },
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin('dist'),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve('./index.html')
-    }),
-    new WorkboxPlugin.GenerateSW()
-  ],
-  devServer: {
-    historyApiFallback: true,
-    overlay: true,
-    open: true,
-    host: 'localhost',
-    port: 3000,
-    contentBase: 'dist',
-    openPage: 'index.html',
-    proxy: {
-      '/api': {
-        target: 'ws://localhost:5000',
-        ws: true,
-        secure: false
-      },
+module.exports = (env, { mode }) => {
+  const devMode = mode !== 'production';
+  const config = {
+    mode,
+    entry: ['./src/js/main.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
     },
-  }
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  autoprefixer({
+                    browsers: ['>1%', 'last 4 versions']
+                  })
+                ]
+              }
+            },
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin('dist'),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      }),
+      new HtmlWebpackPlugin({
+        template: path.resolve('./index.html')
+      }),
+      new WorkboxPlugin.GenerateSW()
+    ],
+    devServer: {
+      historyApiFallback: true,
+      compress: true,
+      overlay: true,
+      open: true,
+      host: 'localhost',
+      port: 3000,
+      contentBase: 'dist',
+      openPage: 'index.html',
+      proxy: {
+        '/api': {
+          target: 'ws://localhost:5000',
+          ws: true,
+          secure: false
+        }
+      }
+    }
+  };
+
+  return config;
 };
